f761b1a4b833fbd24045e3703487fdb6e33cd2ea
anshyyy <anshuman9998@gmail.com>
2023-02-05 19:10:38 +0530

    added O-auth logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/loginScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
